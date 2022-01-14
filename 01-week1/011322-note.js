ea7c141ee3ca47ed5fde4090a4bb5309a305c16c
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-13 19:52:24 -0500

    Update 011322-note.js

--- 01-week1/011322-note.js
+++ 01-week1/011322-note.js
@@ -1,1 +1,41 @@
+web port 80 for http and 443 for https (encrypt data from point A to point B)
+
+request response cycle
+client (web broswer) - http request -> server (application server, web server => html file) -> http response (with html) -> client (web broswer)
+
+html css javascript (reachable files)
+bring up contents as small as possible (single page application frameworks)
+
+locking down your server!!! -> no open door due to web security
+
+you can connect to many servers
+
+client (web browser) - web server - application server (application script) - database server
+
+- web server - firewall right before the web server needs a software/hardware solution to guide incoming request to inner system (whether block or pass)
+- needs specialized account in application server to do certain things like read or write
+- client is everywhere, privacy is a concern
+
+
+server-side language
+- php 
+- asp.net
+- python
+- perl
+- jsp
+- ruby on rails
+- javascript
+
+
+Module
+- function, object, variables in the global level
+- modules are js files contains encapsulation (private to local)
+
+design pattern - facade - only export the data we need
+
+modules are like pattern
+
+require - global function //require("./nameOfTheFile") - implies it is js file 
+
+you can not mis-match commonJS and ES6
 
