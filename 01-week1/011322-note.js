94bcc3fb9508d961957e00217c8ed354deb8889e
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-13 22:53:11 -0500

    Update 011322-note.js

--- 01-week1/011322-note.js
+++ 01-week1/011322-note.js
@@ -37,5 +37,31 @@ modules are like pattern
 
 require - global function //require("./nameOfTheFile") - implies it is js file 
 
-you can not mis-match commonJS and ES6
+you can not mix and match commonJS and ES6 //es6 is async, commonJS runs sync
+//commonjs - default
+const express = require('express');
+module.exports = Task;
+
+//es6
+import process from "process";
+export default App;
+
+you can create alias to hide the name
+
+3 tiers - client, server, database
+
+
+global objects like fs (with sync and async functions)
+by default, the data read is returned as a Buffer object (big binary data like images video photoes)
+
+path module - global - server side object can not be used in client side
+os module - like showing total memory, the number of cpu, the state of the system
+util, print formatted strings like time stamp
+
+
+npm - open source modules (good weekly download, strong version)
+express - node build up web server
+
+
+
 
