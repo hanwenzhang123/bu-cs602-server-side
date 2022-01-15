0db5e59c31c71d68590585fbd5dba47fe7ca8096
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-01-14 23:08:46 -0500

    Update 011322-note.js

--- 01-week1/011322-note.js
+++ 01-week1/011322-note.js
@@ -32,29 +32,26 @@ Node Module
 - modules are like pattern
 
 design pattern - facade - only export the data we need
+3 tiers - client, server, database
 
 require - global function //require("./nameOfTheFile") - implies it is js file 
-
-you can not mix and match commonJS and ES6 //es6 is async, commonJS runs sync
+- you can not mix and match commonJS and ES6 
+- es6 is async, commonJS runs sync
+- you can create alias to hide the name 
 //commonjs - default
 const express = require('express');
 module.exports = Task;
-
 //es6
 import process from "process";
 export default App;
 
-you can create alias to hide the name 
-
-3 tiers - client, server, database
-
-
-global objects like fs (with sync and async functions)
-by default, the data read is returned as a Buffer object (big binary data like images video photoes)
-
-path module - global - server side object can not be used in client side
+Modules are global objects like fs (with sync and async functions) 
+  - server side object can not be used in client side
+  - by default, the data read is returned as a Buffer object (big binary data like images video photoes
+fs module - file system, read and write files
+path module - interact with path easily, provides a way of working with directories and file paths
 os module - like showing total memory, the number of cpu, the state of the system
-util, print formatted strings like time stamp
+util module, print formatted strings like time stamp
 
 
 npm - open source modules (good weekly download, strong version)
