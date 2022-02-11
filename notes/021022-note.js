253b48c08f89f0a1c73d8f655daf78a1333b6664
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-02-10 20:32:35 -0500

    Create 021022-note.js

---
+++ notes/021022-note.js
@@ -0,0 +1,27 @@
+include
+require
+include once  //only load once
+require once  //only load once
+
+static can only access to static, you have to create static function to access to the static
+static is not instance variables, can not be changed through non-static
+static belongs to the no instance of the class, shares along with the whole instance, not for instance variables
+
+by default, interface is public
+folder for interface, a folder for class
+
+class Square implements Shape {}
+class Triangle implements Shape {}
+
+namespace
+
+PDO vs MySQLi
+
+MVC
+model view controller
+model - data/object
+view - what the user sees, UI
+controller - CRUD
+
+PHP has abstract classes and methods. 
+Classes defined as abstract cannot be instantiated, and any class that contains at least one abstract method must also be abstract. 
