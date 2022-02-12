ba1fd67be7112c806a2eced738eedefc4069510a
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-02-12 09:18:36 -0500

    Update 020322-note.js

--- notes/020322-note.js
+++ notes/020322-note.js
@@ -1,1 +1,12 @@
 <?php phpinfo(); ?>
+
+isset() => we have a value then we can generate the rest of the function
+
+if (isset($_POST['netIncome'])){}
+
+PHP Superglobal - Super global variables are built-in variables that are always available in all scopes.
+- $GLOBALS: used to access global variables from anywhere in the PHP script
+- $_SERVER: holds information about headers, paths, and script locations.
+- $_REQUEST: collect data after submitting an HTML form.
+- $_POST: collect form data after submitting an HTML form with method="post"
+- $_GET: collect form data after submitting an HTML form with method="get"
