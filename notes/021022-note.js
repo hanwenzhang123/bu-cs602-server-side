719c8d9eb8aeb323a4404c3efa965b0d76db9f82
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-02-12 10:01:33 -0500

    Update 021022-note.js

--- notes/021022-note.js
+++ notes/021022-note.js
@@ -1,19 +1,38 @@
+Encapsulation - not effective outside of the class, not public exposed like ssn
+Abstraction - model something, further refine/generalize the concept
+Inheritance
+Polymorphism 
+
+A class is a blueprint
+class defines the attributes and behaviors that all objects derived from the class will have
+class must define the basic building blocks of the objects
+
+instance is when you create something based on that class
+  
+constructor -> things you want to assign to your attributes within your class, for immutability if with no setter
+  
+
 include
 require
 include once  //only load once
 require once  //only load once
 
+
 static can only access to static, you have to create static function to access to the static
 static is not instance variables, can not be changed through non-static
 static belongs to the no instance of the class, shares along with the whole instance, not for instance variables
 
+
 by default, interface is public
 folder for interface, a folder for class
+
 
 class Square implements Shape {}
 class Triangle implements Shape {}
 
+
 namespace
+
 
 PDO vs MySQLi
 
@@ -23,5 +42,8 @@ model - data/object
 view - what the user sees, UI
 controller - CRUD
 
+
 PHP has abstract classes and methods. 
 Classes defined as abstract cannot be instantiated, and any class that contains at least one abstract method must also be abstract. 
+
+ 
